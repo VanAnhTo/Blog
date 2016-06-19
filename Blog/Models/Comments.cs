59d98c733d25e075cef23339ca3ac4c07ae07432
VanAnhTo <vananhto.bg@gmail.com>
2016-06-20 06:44:58 +0700

    Comment // chua xong

--- Blog/Models/Comments.cs
+++ Blog/Models/Comments.cs
@@ -8,8 +8,10 @@ namespace Blog.Models
 {
     public class Comments
     {
+        public int CommentId { get; set; }
         public int userId { get; set; }
+        public int PostId { get; set; }
         public string content { get; set; }
-        public DateTime CreatedDate { get; set; }
+        public System.DateTime CreatedDate { get; set; }
     }
 }
